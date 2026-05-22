1036ffc6dcde9ce6ade454063b8e5437221486b6
Valdniro Albino <Leopoldinoalbino12@hotmail.com>
2026-05-22 22:40:55 +0100

    Menu By: Antonio Fontes

--- Tdl/src/App.java
+++ Tdl/src/App.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        Scanner ent = new Scanner(System.in);
+        Lista list = new Lista();
+
+        System.out.println("====== MENU TO-DO-LIST ======");
+        System.out.println("1. Adicionar Tarefa");
+        System.out.println("2. Ver Tarefas");
+        System.out.println("0. Sair");
+        System.out.print("Selecione a opção: ");
+        int op = ent.nextInt();
+        ent.nextLine();  
+        do{
+            switch (op) {
+                 case 1:
+                    System.out.println("ID da tarefa: ");
+                    int id = ent.nextInt();
+                    ent.nextLine();  
+                    System.out.print("Descrição da tarefa: ");
+                    String descricao = ent.nextLine();
+                    System.out.print("Data de vencimento (dd/mm/yyyy): ");
+                    String data = ent.nextLine();
+                    System.out.print("Prioridade (baixa/média/alta): ");
+                    String prioridade = ent.nextLine();
+                    Tarefas task = new Tarefas(id, descricao, data, prioridade);
+                    list.inserir(task);
+                 break;
+                 case 2:
+                    System.out.println("A ver tarefas...");
+                    list.ver();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }while (op!=0); 
 
 
     }
