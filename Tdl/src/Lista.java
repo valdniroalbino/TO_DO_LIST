fddcf92527bd3c02559c125b64c8f1a139c65339
Valdniro Albino <Leopoldinoalbino12@hotmail.com>
2026-06-09 23:11:11 +0100

    mexi nas minhas funcoes e alterei o nome de algumas funcooes e parametros

--- Tdl/src/Lista.java
+++ Tdl/src/Lista.java
@@ -8,20 +8,34 @@ public class Lista {
         this.tarefas = new ArrayList<>();
     }
 
-    public void inserir(Tarefas tarefas){
-        
+    public void adicionarTarefa(Tarefas t){
+        if(t == null){
+            System.out.println("tarefa invalida!");
+            return;
+        }
+        tarefas.add(t);
+        System.out.println("tarefa adicionada com sucesso!");
     }
 
-    public void ver(){
-       
+    public void listarTarefas(){
+        System.out.println("LISTA DE TAREFAS: ");
+       for(int i = 0; i < tarefas.size(); i++){
+        System.out.printf("%dº %s;\n",i+1,tarefas.get(i).getTitulo());
+       }
     }
 
     public void atualizar(int id, String campo, String novoValor) {
         
     }
 
-    public void remover(int id) {
-        
+    public void removerTarefa(int indice) {
+        if(indice >= 0 && tarefas.size() > indice){
+            tarefas.remove(indice);
+
+            System.out.println("tarefa removida com sucesso!");
+        }else{
+            System.out.println("indice invalido!");
+        } 
     }
 
     public ArrayList<Tarefas> obterTarefas() {
